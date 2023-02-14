a49a2ccde27434efef4e85c6edb0066366158281
1000mj <cjsaudwls12@gmail.com>
2023-02-14 19:04:22 +0900

    페이지분리,초기UI작업 02

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import { AppBar,Toolbar } from "@mui/material";
-import { Routes ,Route,Navigate,useLocation, NavLink} from "react-router-dom";
+import { AppBar, Toolbar } from "@mui/material";
+import {
+  Routes,
+  Route,
+  Navigate,
+  useLocation,
+  NavLink,
+} from "react-router-dom";
 
 import MainPage from "./pages/MainPage";
-import Sub1Page from "./pages/Sub1Page";
-
-
+import WritePage from "./pages/WritePage";
+import NoticeSnackbar from "./components/NoticeSnackbar";
 
 function App() {
   const location = useLocation();
@@ -14,21 +19,26 @@ function App() {
       <AppBar position="static">
         <Toolbar>
           <div className="flex-1"></div>
-          <span className="font-bold">앱 이름</span>
+          <span className="font-bold select-none">앱 이름</span>
           <div className="flex-1 flex justify-end">
-            {location.pathname != "/sub1"&&
-            (<NavLink to="/sub1">서브1</NavLink>
+            {location.pathname != "/write" && (
+              <NavLink to="/write" className="select-none">
+                글쓰기
+              </NavLink>
             )}
-            {location.pathname != "/main"&&
-            (<NavLink to="/main">이전</NavLink>
+            {location.pathname == "/write" && (
+              <NavLink to="/main" className="select-none">
+                이전
+              </NavLink>
             )}
           </div>
         </Toolbar>
       </AppBar>
+      <NoticeSnackbar />
       <Routes>
-        <Route path="/main" element={<MainPage/>}/>
-        <Route path="/sub1" element={<Sub1Page/>}/>
-        <Route path="*" element={<Navigate to="/main" />}/>
+        <Route path="/main" element={<MainPage />} />
+        <Route path="/write" element={<WritePage />} />
+        <Route path="*" element={<Navigate to="/main" />} />
       </Routes>
     </>
   );
